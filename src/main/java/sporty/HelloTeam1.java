99c94e59bc73d1a8e8a0d4aef531fcb14ff07af0
gewure <jasperreichardt@gmail.com>
2015-10-22 11:38:58 +0200

    Jenkins <-> Github test

--- src/main/java/sporty/HelloTeam1.java
+++ src/main/java/sporty/HelloTeam1.java
@@ -6,5 +6,6 @@ public class HelloTeam1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, Team 1!! :D");
 		System.out.println("Hallo");
+		System.out.println("Jenkins Test");
 	}
 }
