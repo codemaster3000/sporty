c7d0f11aa218fa5cc06f37898806480336fe0cc9
Karoline <Karoline@KaroLappi>
2015-10-29 14:55:21 +0100

    NewMemberController

--- jFXClient/src/main/java/at/sporty/team1/presentation/controllers/MainViewController.java
+++ jFXClient/src/main/java/at/sporty/team1/presentation/controllers/MainViewController.java
@@ -1,5 +1,25 @@
 package at.sporty.team1.presentation.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
 public class MainViewController implements IJfxController {
+	@FXML
+	private TabPane _tabPanel;
+	
 	// commit test after new eclipse installation - Carola
+	
+	@FXML
+	private void openNewMemberView()
+	{
+		
+		//ViewLoader viewLoader = ViewLoader.loadView(NewMemberController.class);	
+		//Node node = viewLoader.loadNode();
+		//Tab t = new Tab(); // oder new Tab(node);
+		// t.loadNode(node);
+		//NewMemberController c = viewLoader.getController();
+		
+		//_tabPanel.getTabs().add(t);
+		
+	}
 }
---
+++ jFXClient/src/main/java/at/sporty/team1/presentation/controllers/NewMemberController.java
@@ -0,0 +1,8 @@
+package at.sporty.team1.presentation.controllers;
+
+import javafx.fxml.FXML;
+
+public class NewMemberController implements IJfxController{
+
+	
+}
