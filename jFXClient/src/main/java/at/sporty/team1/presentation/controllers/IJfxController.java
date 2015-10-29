58eb5ab3ac5bfda841bc3c0c3d46122780b37030
sereGkaluv <malicx7@gmail.com>
2015-10-29 21:25:43 +0100

    unused imports are removed

--- jFXClient/src/main/java/at/sporty/team1/presentation/controllers/IJfxController.java
+++ jFXClient/src/main/java/at/sporty/team1/presentation/controllers/IJfxController.java
@@ -1,14 +1,9 @@
 package at.sporty.team1.presentation.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.Initializable;
 
 /*
  * This is a "Marker interface" for all JavaFX UI Controllers.
  */
 public interface IJfxController extends Initializable {
-
-	
 }
