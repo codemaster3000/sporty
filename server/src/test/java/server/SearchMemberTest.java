02476056aa9f0c43165e5655781a14eb08f134c1
Karoline <Karoline@KaroLappi>
2015-11-01 20:46:13 +0100

    Junit Test searchMember

--- server/src/test/java/server/SearchMemberTest.java
+++ server/src/test/java/server/SearchMemberTest.java
@@ -1,14 +1,76 @@
 package server;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import at.sporty.team1.application.controller.MemberController;
+import at.sporty.team1.rmi.dtos.MemberDTO;
+
 
 public class SearchMemberTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	/**
+	 * One Member found with fName Claudia
+	 */
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void searchMemberTest_1() {
+		
+		String searchString = "Claudia";
+		MemberController mem = null;
+		List<MemberDTO> members = new ArrayList<>();
+		
+		try {
+			mem = new MemberController();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			members = mem.searchForMembers(searchString);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(1, members.size());
+	}
+	
+	/**
+	 * One Member found with fName Claudia
+	 */
+	@Test
+	public void searchMemberTest_2() {
+		
+		String searchString = "Claudia";
+		MemberController mem = null;
+		List<MemberDTO> members = new ArrayList<>();
+		
+		try {
+			mem = new MemberController();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			members = mem.searchForMembers(searchString);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(1, members.size());
 	}
 
 }
