f477172c08e157e7832e90e090daec001434e19a
gewure <jasperreichardt@gmail.com>
2015-11-03 15:46:51 +0100

    DepartmentDAO!

--- server/src/main/java/at/sporty/team1/persistence/daos/DepartmentDAO.java
+++ server/src/main/java/at/sporty/team1/persistence/daos/DepartmentDAO.java
@@ -1,6 +1,11 @@
 package at.sporty.team1.persistence.daos;
 
 import at.sporty.team1.domain.Department;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
 
 /**
  * Created by f00 on 30.10.15.
@@ -12,6 +17,14 @@ public class DepartmentDAO extends HibernateGenericDAO<Department> {
      */
     public DepartmentDAO() {
         super(Department.class);
+    }
+
+    public List<Department> getDepartmentBySport(String sport) {
+        Criterion criterion;
+
+        criterion = Restrictions.or(Restrictions.like("sport", sport, MatchMode.ANYWHERE));
+
+        return super.findByCriteria(criterion);
     }
 
 }
