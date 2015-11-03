5aa8dc2a449d7b40a7f2231b1e579c23f2f9e1fa
gewure <jasperreichardt@gmail.com>
2015-11-03 13:51:09 +0100

    +TeamDAO +DepartmentDAO

--- server/src/main/java/at/sporty/team1/persistence/daos/DepartmentDAO.java
+++ server/src/main/java/at/sporty/team1/persistence/daos/DepartmentDAO.java
@@ -1,7 +1,17 @@
 package at.sporty.team1.persistence.daos;
 
+import at.sporty.team1.domain.Department;
+
 /**
  * Created by f00 on 30.10.15.
  */
-public class DepartmentDAO {
+public class DepartmentDAO extends HibernateGenericDAO<Department> {
+
+    /**
+     * Constructor
+     */
+    public DepartmentDAO() {
+        super(Department.class);
+    }
+
 }
