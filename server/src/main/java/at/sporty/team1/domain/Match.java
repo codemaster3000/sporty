c3ae6377904fdaec49ef8419eb9460425213db4e
gewure <jasperreichardt@gmail.com>
2015-11-18 11:17:18 +0100

    MatchEntity and Interfaces done

--- server/src/main/java/at/sporty/team1/domain/Match.java
+++ server/src/main/java/at/sporty/team1/domain/Match.java
@@ -3,17 +3,13 @@ package at.sporty.team1.domain;
 import at.sporty.team1.domain.interfaces.IMatch;
 import at.sporty.team1.misc.converters.SQLDateConverter;
 
-import java.util.Date;
+import javax.persistence.*;
 
-import javax.persistence.Basic;
-import javax.persistence.Convert;
-
-/**
- * Created by f00 on 03.11.15.
- *  TODO hibernatemapping
- */
+@Entity
+@Table(name = "match")
 public class Match implements IMatch {
 
+    public Integer matchId;
     public String date;
     public String team1;
     public String team2;
@@ -22,57 +18,99 @@ public class Match implements IMatch {
     public String referee;
 
     @Override
+    @Id
+    @GeneratedValue
+    @Column(name = "matchId")
+    public Integer getMatchId() {
+        return matchId;
+    }
+
+    @Override
+    public void setMatchId(Integer matchId) {
+        this.matchId = matchId;
+    }
+
+    @Override
+    @Basic
+    @Column(name = "matchResult")
+    public String getMatchResult() {
+        return matchResult;
+    }
+
+    @Override
+    public void setMatchResult(String matchResult) {
+        this.matchResult = matchResult;
+    }
+
+
+    @Override
     @Basic
     @Convert(converter = SQLDateConverter.class)
     public String getDate() {
         return date;
     }
 
+    @Override
     public void setDate(String date) {
         this.date = date;
     }
 
     @Override
+    @Basic
+    @Column(name = "team2")
     public String getTeam2() {
         return team2;
     }
 
+    @Override
     public void setTeam2(String team2) {
         this.team2 = team2;
     }
 
     @Override
+    @Basic
+    @Column(name = "team1")
     public String getTeam1() {
         return team1;
     }
 
+    @Override
     public void setTeam1(String team1) {
         this.team1 = team1;
     }
 
     @Override
+    @Basic
+    @Column(name = "location")
     public String getLocation() {
         return location;
     }
 
+    @Override
     public void setLocation(String location) {
         this.location = location;
     }
 
     @Override
+    @Basic
+    @Column(name = "result")
     public String getResult() {
         return matchResult;
     }
 
+    @Override
     public void setResult(String matchResult) {
         this.matchResult = matchResult;
     }
 
     @Override
+    @Basic
+    @Column(name = "referee")
     public String getReferee() {
         return referee;
     }
 
+    @Override
     public void setReferee(String referee) {
         this.referee = referee;
     }
