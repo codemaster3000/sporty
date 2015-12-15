459aaeee60695aca042f0b8eabc4e9e202f37a15
CarolaFHV <carola.nagel@students.fhv.at>
2015-12-15 19:23:57 +0100

    updated tournament POJO and website

--- webclient/src/main/java/at/sporty/team1/web/TournamentPOJO.java
+++ webclient/src/main/java/at/sporty/team1/web/TournamentPOJO.java
@@ -6,50 +6,73 @@ import java.util.List;
 import javax.ejb.EJB;
 
 import at.sporty.team1.shared.api.ejb.ITournamentControllerEJB;
-import at.sporty.team1.shared.dtos.DepartmentDTO;
 import at.sporty.team1.shared.dtos.TournamentDTO;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.Id;
 
-public class TournamentPOJO {
-	
-	private List<TournamentDTO> _tournamentList;
-	@EJB
+@ManagedBean
+@RequestScoped
+@Named(value = "tournamentPOJO")
+public class TournamentPOJO implements Serializable {
+
+    /**
+     * default ID
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    private Long id;
+
+    private List<TournamentDTO> tournamentList;
+
+    @EJB
 	private ITournamentControllerEJB _tournamentController;
 
-	public TournamentPOJO (){
-		_tournamentList = _tournamentController.searchAllTournaments();
-	}
-	
-	public List<TournamentDTO> getAllTournaments (){
-		return _tournamentList;
-	}
-	
-	public List<String> getTournamentDates(){
-		List<String> dates = new ArrayList<String>();
-		
-		for (TournamentDTO t : _tournamentList) {
-			dates.add(t.getDate());
-		}
-		
-		return dates;
-	}
-	
-	public List<String> getTournamentLocations(){
-		List<String> locations = new ArrayList<String>();
-		
-		for (TournamentDTO t : _tournamentList) {
-			locations.add(t.getLocation());
-		}
-		
-		return locations;
-	}
-	
-	public List<String> getTournamentSports(){
-		List<String> sports = new ArrayList<String>();
-		
-		for (TournamentDTO t : _tournamentList) {
-			sports.add(t.getDepartment().getSport());
-		}
-		
-		return sports;
-	}
+    public TournamentPOJO() {
+        tournamentList = _tournamentController.searchAllTournaments();
+    }
+
+    public List<TournamentDTO> getAllTournaments() {
+        return tournamentList;
+    }
+
+    public List<String> getTournamentDates() {
+        List<String> dates = new ArrayList<String>();
+
+        for (TournamentDTO t : tournamentList) {
+            dates.add(t.getDate());
+        }
+
+        return dates;
+    }
+
+    public List<String> getTournamentLocations() {
+        List<String> locations = new ArrayList<String>();
+
+        for (TournamentDTO t : tournamentList) {
+            locations.add(t.getLocation());
+        }
+
+        return locations;
+    }
+
+    public List<String> getTournamentSports() {
+        List<String> sports = new ArrayList<String>();
+
+        for (TournamentDTO t : tournamentList) {
+            sports.add(t.getDepartment().getSport());
+        }
+
+        return sports;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
